d1e9f8081696e1c361392249836d68c1c8e31727
ajlopez <webmaster@ajlopez.com>
2013-05-18 17:35:32 -0300

    Add Integers, first test

---
+++ Src/Obops.Tests/OperatorsTests.cs
@@ -0,0 +1,20 @@
+﻿namespace Obops.Tests
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class OperatorsTests
+    {
+        [TestMethod]
+        public void AddIntegers()
+        {
+            Assert.AreEqual(3, Operators.AddObject(1, 2));
+            Assert.AreEqual(-1, Operators.AddObject(1, -2));
+            Assert.AreEqual(0, Operators.AddObject(0, 0));
+        }
+    }
+}
---
+++ Src/Obops/Operators.cs
@@ -0,0 +1,15 @@
+﻿namespace Obops
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public static class Operators
+    {
+        public static object AddObject(object left, object right)
+        {
+            return (int)left + (int)right;
+        }
+    }
+}
